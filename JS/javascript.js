afa2a7ed24a260e48b199a6af83ea17f383deba5
ItzelCtrl-z <it.paniagua.s@gmail.com>
2024-11-05 20:12:57 -0600

    ya está la base de la promera página del sitio

---
+++ JS/javascript.js
@@ -0,0 +1,7 @@
+//manda a llamar el botón sí 
+const btnSi = document.getElementById ("BotónS")
+//manda a llamar el botón sí 
+const btnNo = document.getElementById ("BotónN")
+//manda llamar al home
+
+//manda llamar a la página externa
